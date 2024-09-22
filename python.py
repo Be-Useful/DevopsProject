dc70055920fd7ad52d10d7fa17900c8471346a3f
Veeraj Singh <139060110+veeraj-singh@users.noreply.github.com>
2024-09-22 15:50:12 +0530

    Update python.py

--- python.py
+++ python.py
@@ -233,3 +233,14 @@ else:
        factorial = factorial*i
    print("The factorial of",num,"is",factorial)
 
+# I am veeraj i am adding my contribution
+# Python Program to calculate the square root
+
+# Note: change this value for a different result
+num = 8 
+
+# To take the input from the user
+#num = float(input('Enter a number: '))
+
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
